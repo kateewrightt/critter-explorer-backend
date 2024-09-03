b1b7d1210e7d29e85862e7c1c7c54c66b0dcfc9f
kateewrightt <wrighk@outlook.com>
2024-09-03 14:44:35 +1000

    remove pagecount

--- routes/pageCount.js
+++ routes/pageCount.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const router = express.Router();
-const {
-  ensureBucketExists,
-  ensureCounterObjectExists,
-  incrementPageCount,
-} = require("../aws");
+// const express = require("express");
+// const router = express.Router();
+// // const {
+// //   ensureBucketExists,
+// //   ensureCounterObjectExists,
+// //   incrementPageCount,
+// // } = require("../aws");
 
-ensureBucketExists();
+// // ensureBucketExists();
 
-router.get("/", async (req, res) => {
-  try {
-    // Ensure counter object exists (ensureCounterObjectExists)
-    await ensureCounterObjectExists();
+// router.get("/", async (req, res) => {
+//   try {
+//     // Ensure counter object exists (ensureCounterObjectExists)
+//     // await ensureCounterObjectExists();
 
-    // Retrieve current page count (incrementPageCount) and increment it
-    const updatedCount = await incrementPageCount();
+//     // Retrieve current page count (incrementPageCount) and increment it
+//     const updatedCount = await incrementPageCount();
 
-    // Send the updated page count as a response or render it in your HTML template
-    res.status(200).json({ pageCount: updatedCount }); // Assuming JSON response
-  } catch (error) {
-    // Handle errors
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" }); // Example error response
-  }
-});
+//     // Send the updated page count as a response or render it in your HTML template
+//     res.status(200).json({ pageCount: updatedCount }); // Assuming JSON response
+//   } catch (error) {
+//     // Handle errors
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "Internal Server Error" }); // Example error response
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
