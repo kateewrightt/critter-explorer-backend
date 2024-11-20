9086647d0c50615803ebe55ac1e003b78ca8093d
kateewrightt <wrighk@outlook.com>
2024-11-20 13:12:10 +1100

    Serve favicon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var favicon = require("serve-favicon");
 
 var indexRouter = require("./routes/index");
 var citySearchRouter = require("./routes/citySearch");
@@ -21,8 +22,10 @@ app.use(cors({
   credentials: false
 }));
 
+// Serve the favicon
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-// view engine setup
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
@@ -39,12 +42,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
@@ -52,6 +55,5 @@ app.use(function (err, req, res, next) {
     error: req.app.get("env") === "development" ? err : {}
   });
 });
-
 
 module.exports = app;
