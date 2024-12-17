396644ff529425230c7c9f62614b76165b940739
kateewrightt <wrighk@outlook.com>
2024-12-17 15:02:26 +1100

    Add '/all-critters' route

--- app.js
+++ app.js
@@ -19,41 +19,43 @@ import critterImagesRouter from "./routes/flickr-gallery.js";
 import bugsRouter from "./routes/critter-bugs.js";
 import fishRouter from "./routes/critter-fish.js";
 import seaCreaturesRouter from "./routes/critter-sea.js";
+import allCrittersRouter from "./routes/critter-all.js";
 
 const app = express();
 
 // CORS setup
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://critter-explorer.netlify.app',
-    'https://critter-explorer-2.netlify.app',
-    'https://critter-explorer.com',
-    'https://www.critter-explorer.com'
-  ],
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://critter-explorer.netlify.app",
+      "https://critter-explorer-2.netlify.app",
+      "https://critter-explorer.com",
+      "https://www.critter-explorer.com",
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
+  })
+);
 
 // Middleware setup
-app.use(logger("dev"));
-app.use(express.json());
+app.use(logger("dev")); 
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Define routes
-app.use("/", indexRouter); // Root route
+app.use("/", indexRouter); 
 app.use("/citySearch", citySearchRouter);
 app.use("/cityDateTime", cityDateTimeRouter);
 app.use("/critterImages", critterImagesRouter);
 app.use("/bugs", bugsRouter);
 app.use("/fish", fishRouter);
 app.use("/sea-creatures", seaCreaturesRouter);
+app.use("/all-critters", allCrittersRouter);
 
-// Catch 404 and forward to error handler
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
@@ -61,10 +63,11 @@ app.use(function (req, res, next) {
 // Error handler
 app.use(function (err, req, res, next) {
   console.error("Error:", err.message);
+  console.error("Stack:", err.stack);
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: req.app.get("env") === "development" ? err : {},
+    error: req.app.get("env") === "development" ? err : {}, 
   });
 });
 
